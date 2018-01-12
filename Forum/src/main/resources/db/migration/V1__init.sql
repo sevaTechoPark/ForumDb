b84aeea01b9568a813189c0ecab082b46d577d3f
sevaTechoPark <seva213415@inbox.ru>
2018-01-12 11:34:15 +0300

    add parth1 indexes

--- Forum/src/main/resources/db/migration/V1__init.sql
+++ Forum/src/main/resources/db/migration/V1__init.sql
@@ -55,7 +55,9 @@ CREATE TABLE Post(
 
 CREATE INDEX post_thread_id ON Post(thread, id);
 CREATE INDEX post_thread_path ON Post(thread, path);
+CREATE INDEX post_thread_path1 ON Post(thread, (path[1]));
 CREATE INDEX post_id_path ON Post(id, path);
+CREATE INDEX post_id_path1 ON Post(id, (path[1]));
 
 CREATE TABLE Vote(
   userId int4,
@@ -64,7 +66,6 @@ CREATE TABLE Vote(
   FOREIGN KEY (userId) REFERENCES FUser(id),
   FOREIGN KEY (threadId) REFERENCES Thread(id)
 );
-
 
 CREATE TABLE ForumUsers(
   userId int4,
